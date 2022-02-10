ccd807506b5b0072a457d92211953b1a0d6746ff
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-02-10 10:08:49 -0300

    fix(todo-app): fixing accessibility issue

--- challenges/todo-app-main/assets/js/lib/TaskTemplate.js
+++ challenges/todo-app-main/assets/js/lib/TaskTemplate.js
@@ -3,12 +3,12 @@ const TaskTemplate =  (currentTask, index) => {
   <div draggable="true" class="dragglabe">
 
     <div class="status">
-      <span class="check" data-index="${index}"></span>
+      <span class="check" data-index="${index}" role="button" aria-label="Mark task as complete"></span>
     </div>
 
     <span class="currentTask">${currentTask}</span>
 
-    <div class="close" aria-label="Remove item from list">
+    <div class="close" aria-label="Remove item from list" role="button" aria-label="Remove item from list">
       <img src="./assets/images/icon-cross.svg" alt="Remove item from list" data-index="${index}">
     </div>
   </div>
