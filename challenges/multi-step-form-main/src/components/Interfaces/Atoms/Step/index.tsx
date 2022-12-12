70f51a089d1c93003710766396d4efda0aa4362d
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-12-12 15:45:57 -0300

    chore: improving aria-label

--- challenges/multi-step-form-main/src/components/Interfaces/Atoms/Step/index.tsx
+++ challenges/multi-step-form-main/src/components/Interfaces/Atoms/Step/index.tsx
@@ -1,10 +1,14 @@
-import style from './style.module.css';
+import style from "./style.module.css";
 
-export interface StepProps{
-  current: number,
-  state?: 'default' | 'active',
+export interface StepProps {
+  current: number;
+  state?: "default" | "active";
 }
 
-export function Step({current, state = 'default'}: StepProps){
-  return <span className={style.step} data-state={state} aria-label="step">{current}</span>
+export function Step({ current, state = "default" }: StepProps) {
+  return (
+    <span aria-label={`Step number indicator`} className={style.step} data-state={state}>
+      {current}
+    </span>
+  );
 }
